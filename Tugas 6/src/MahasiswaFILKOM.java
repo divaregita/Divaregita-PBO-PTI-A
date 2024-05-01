ab3ace4e4cac8bb1cae45a04b316068af71f7ae8
divaregita <160575900+divaregita@users.noreply.github.com>
2024-05-01 21:16:11 +0700

    MahasiswaFILKOM.java

---
+++ Tugas 6/src/MahasiswaFILKOM.java
@@ -0,0 +1,72 @@
+public class MahasiswaFILKOM extends Manusia {
+    private String nim;
+    private double ipk;
+
+    // Constructor
+    public MahasiswaFILKOM(String nama, boolean jenisKelamin, String nik, boolean menikah, String nim, double ipk) {
+        super(nama, jenisKelamin, nik, menikah);
+        this.nim = nim;
+        this.ipk = ipk;
+    }
+
+    // Getter and Setter methods
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public double getIpk() {
+        return ipk;
+    }
+
+    public void setIpk(double ipk) {
+        this.ipk = ipk;
+    }
+
+    // Method to get status
+    public String getStatus() {
+        String angkatan = nim.substring(0, 2);
+        String prodi = "";
+
+        switch (nim.charAt(6)) {
+            case '2':
+                prodi = "Teknik Informatika";
+                break;
+            case '3':
+                prodi = "Teknik Komputer";
+                break;
+            case '4':
+                prodi = "Sistem Informasi";
+                break;
+            case '6':
+                prodi = "Pendidikan Teknologi Informasi";
+                break;
+            case '7':
+                prodi = "Teknologi Informasi";
+                break;
+            default:
+                prodi = "Prodi tidak valid";
+        }
+
+        return prodi + ", " + "20" + angkatan;
+    }
+
+    // Method to get beasiswa
+    public double getBeasiswa() {
+        if (ipk >= 3.0 && ipk <= 3.5) {
+            return 50;
+        } else if (ipk > 3.5 && ipk <= 4) {
+            return 75;
+        } else {
+            return 0; // No scholarship
+        }
+    }
+
+    // toString method
+    public String toString() {
+        return super.toString() + "\nNIM: " + nim + "\nIPK: " + ipk + "\nStatus: " + getStatus();
+    }
+}
